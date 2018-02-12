bdc1d14b665bf4ab729d9228d43155262cd70e78
dew95531 <dew_95531@windowslive.com>
2018-02-12 22:16:35 +0700

    [Update] user.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 var app = require('express')();
 
 /* ใช้ port 7777 หรือจะส่งเข้ามาตอนรัน app ก็ได้ */
-var port = process.env.PORT || 7777;
+var port = process.env.PORT || 3001;
 
+var users = require('./users');
 /* Routing */
 app.get('/', function (req, res) {
     res.send('<h1>Hello Node.js</h1>');
@@ -11,6 +12,14 @@ app.get('/', function (req, res) {
 app.get('/index', function (req, res) {
     res.send('<h1>This is index page</h1>');
 });
+app.get('/users', function (req, res){
+    res.json(users.findAll());
+});
+app.get('/users/:id', function (req, res){
+    var id = req.params.id;
+    res.json(users.findById(id));
+});
+
 
 /* สั่งให้ server ทำการรัน Web Server ด้วย port ที่เรากำหนด */
 app.listen(port, function() {
